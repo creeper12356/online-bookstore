caa63dcbfd040882693f023d2e883cbaf5450e8c
creeper <creeperhjt@sjtu.edu.cn>
2024-06-01 18:22:26 +0800

    订单页面支持查看价格和数量

--- src/components/OrderTableItem.jsx
+++ src/components/OrderTableItem.jsx
@@ -1,4 +1,4 @@
-import { Table, TableHead, TableCell, TableBody, List, ListItem, ListItemText, TableRow, Collapse, Button, SpeedDial, SpeedDialIcon, Fab, Box, Divider } from "@mui/material";
+import { TableHead, TableCell, TableBody, List, ListItem, ListItemText, TableRow, Collapse, Button, SpeedDial, SpeedDialIcon, Fab, Box, Divider, Typography, Link } from "@mui/material";
 import { useState } from "react";
 import React from "react";
 
@@ -30,11 +30,21 @@ const OrderTableItem = ({ order }) => {
                 order.books.map(
                     book => (
                         <React.Fragment key={book.id}>
-                            <ListItem style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start'}}>
-                                <ListItemText primary={book.title} secondary={book.author} />
-                                <Box>
-                                    <img src={book.cover} alt={book.title} style={{height: 150, width: 'auto'}}/>
-                                </Box>
+                            <ListItem style={{ display: 'flex', flexDirection: 'row', gap: 5}}>
+                                <div>
+                                    <img src={book.cover} alt={book.title} style={{width: 150}}/>
+                                </div>
+                                <div style={{display: 'flex', flexDirection: 'row', alignItems: "center", gap: 10}}>
+                                    <Link
+                                        href={`/book/${book.bookId}`}
+                                        style={{textDecoration: 'none'}}
+                                    >
+                                        { book.title }
+                                    </Link>
+                                    <Typography variant="body1" color="darkblue">{`x${book.number}`}</Typography>
+                                </div>
+                                <Box flexGrow={1} />
+                                <Typography variant="h6" color="red">{`¥${book.price / 100} `}</Typography><Typography>/本</Typography>
                             </ListItem>
                             <Divider />
                         </React.Fragment>
